27a41c1561f0a7d943bef0bfc3f5bbb5cabfe573
Soomi Jang <109soomi@naver.com>
2020-07-29 14:57:02 +0900

    Scence+UI 1차_정은
    Scence+UI 1차_정은

---
+++ OUGI/Assets/SceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    #region Auth
+    public void GotoSignUp()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SignUp");
+    }
+    public void GotoLogin()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Login");
+    }
+    #endregion
+
+    #region UI Button
+
+    public void GotoLobby()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lobby");
+    }
+    public void GotoGameMap()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameMap");
+    }
+    public void GotoQuest()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Quest");
+    }
+    public void GotoAlbum()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Album");
+    }
+    public void GotoShop()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Shop");
+    }
+
+    #endregion
+}
